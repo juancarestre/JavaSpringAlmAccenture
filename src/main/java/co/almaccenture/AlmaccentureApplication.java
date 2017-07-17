a3535230a58f9837fb4153d8f5b3d78bf3a70e88
00Fede <feedkiko@gmail.com>
2017-07-17 15:20:04 -0500

    first commit

---
+++ src/main/java/co/almaccenture/AlmaccentureApplication.java
@@ -0,0 +1,12 @@
+package co.almaccenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmaccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmaccentureApplication.class, args);
+	}
+}
