bd1a6555ecfc9116bf376c88876594b000434d7c
00Fede <feedkiko@gmail.com>
2017-07-18 21:12:35 -0500

    Adelanto a Controller

--- src/main/java/co/almaccenture/controller/ControladorVentaImpl.java
+++ src/main/java/co/almaccenture/controller/ControladorVentaImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.QueryParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -52,7 +53,8 @@ public class ControladorVentaImpl implements ControladorVenta {
 
 	@RequestMapping(value = "/ventas", method=RequestMethod.GET, params={"id","cant"})
 	@Override
-	public ModelAndView ingresarProducto(String idProducto, int cantidad) {
+	public ModelAndView ingresarProducto(@QueryParam("id") String idProducto,
+			@QueryParam("cant") int cantidad) {
 		
 		DetalleVenta producto = ventaBl.agregarDetalleVenta(idProducto, cantidad);
 		
@@ -62,16 +64,24 @@ public class ControladorVentaImpl implements ControladorVenta {
 		return new ModelAndView("redirect:/");
 	}
 
-	
+	@RequestMapping(value = "/ventas/del", method=RequestMethod.GET, params={"id"})
 	@Override
 	public ModelAndView eliminarProducto(HttpServletRequest req, RedirectAttributes redirect) {
-		// TODO Auto-generated method stub
-		return null;
+		String idProducto = req.getParameter("id");
+		for (DetalleVenta detalleVenta : productos) {
+			if(detalleVenta.getProducto().getIdProducto().equals(idProducto))
+				productos.remove(detalleVenta);
+		}
+		return new ModelAndView("redirect:/");
 	}
-
+	
+	@RequestMapping(value = "/ventas", method=RequestMethod.POST, params="save")
 	@Override
 	public ModelAndView confirmarVenta() {
-		// TODO Auto-generated method stub
+		//TODO: Popup de confirmación
+		
+		venta.setDetalles(productos);
+		ventaBl.guardarVenta(venta);
 		return null;
 	}
 
