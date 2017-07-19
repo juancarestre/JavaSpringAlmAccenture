40acd505a7ca16a91adb5ad1413689b97cc0283a
00Fede <feedkiko@gmail.com>
2017-07-19 14:24:27 -0500

    findOne en Repositorio Venta

--- src/main/java/co/almaccenture/repository/RepositorioVenta.java
+++ src/main/java/co/almaccenture/repository/RepositorioVenta.java
@@ -1,11 +1,15 @@
 package co.almaccenture.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import co.almaccenture.model.Venta;
 
+@Repository
 public interface RepositorioVenta extends CrudRepository<Venta, Integer> {
 
 	public Venta findByIdVentaAndEstadoVenta(Integer idVenta, int estadoVenta);
 	
+	public Venta findOne(Integer idVenta);
+		
 }
