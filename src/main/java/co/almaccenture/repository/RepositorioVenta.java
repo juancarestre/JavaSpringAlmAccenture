4ebc869c8338fdaa1d90bddc24d5e9fe3164fa43
sisaza92 <sisaza92@gmail.com>
2017-07-19 10:56:01 -0500

    Cambio menor en el tipo de dato del parámetro estadoVenta

--- src/main/java/co/almaccenture/repository/RepositorioVenta.java
+++ src/main/java/co/almaccenture/repository/RepositorioVenta.java
@@ -6,6 +6,6 @@ import co.almaccenture.model.Venta;
 
 public interface RepositorioVenta extends CrudRepository<Venta, Integer> {
 
-	public Venta findByIdVentaAndEstadoVenta(Integer idVenta, int estadoVenta);
+	public Venta findByIdVentaAndEstadoVenta(Integer idVenta, boolean estadoVenta);
 	
 }
