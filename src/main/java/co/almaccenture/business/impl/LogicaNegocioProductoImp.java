a29220d3d2fccecdbeb8d927d76de7e720001200
SaraPavas <sara.pavas@udea.edu.co>
2017-07-21 13:43:46 -0500

    Lógica de negocio para producto: métodos para resta de productos y verificación de productos próximos a agostarse. Se incluyó el método de resta a la lógica de negocios de guardarVenta

---
+++ src/main/java/co/almaccenture/business/impl/LogicaNegocioProductoImp.java
@@ -0,0 +1,47 @@
+package co.almaccenture.business.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.almaccenture.business.LogicaNegocioProducto;
+import co.almaccenture.exception.LogicaNegocioExcepcion;
+import co.almaccenture.model.Producto;
+import co.almaccenture.repository.RepositorioProducto;
+
+@Service
+public class LogicaNegocioProductoImp implements LogicaNegocioProducto  {
+	
+	
+	
+	@Autowired
+	private RepositorioProducto repositorioProducto;
+	
+	public static final int UMBRAL = 10;	
+	public static final String MENSAJE_ALERTA_PRODUCTO_ESCASO = "Alerta: Este producto tiene pocas existencias en inventario.";
+	public static final String MENSAJE_PRODUCTO_NO_ENCONTRADO = "El Producto con el codigo especificado no existe.";
+	
+	@Override
+	public void restarProductos(Producto producto, int cantidad) throws LogicaNegocioExcepcion {
+		
+		Integer stock;
+		stock=producto.getCantidadProducto();
+		
+		producto.setCantidadProducto(stock-cantidad);
+		repositorioProducto.save(producto);
+		
+	}
+	@Override
+	public boolean verificarCantidadProducto(Producto producto)throws LogicaNegocioExcepcion{
+		
+		if(producto == null){throw new LogicaNegocioExcepcion(MENSAJE_PRODUCTO_NO_ENCONTRADO);}
+		
+		Integer stock = producto.getCantidadProducto();
+		return stock <= UMBRAL;
+		
+	}
+	
+	
+		
+	
+
+}
