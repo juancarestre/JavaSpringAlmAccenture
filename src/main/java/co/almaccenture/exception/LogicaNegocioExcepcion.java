05635e5a39eedef7b719273809d074c7dc04407a
SaraPavas <sara.pavas@udea.edu.co>
2017-07-21 18:28:09 -0500

    Modificación de la clase excepciones y lógica de negocio para un campo vacío

--- src/main/java/co/almaccenture/exception/LogicaNegocioExcepcion.java
+++ src/main/java/co/almaccenture/exception/LogicaNegocioExcepcion.java
@@ -5,11 +5,22 @@ import org.apache.log4j.Logger;
 @SuppressWarnings("serial")
 public class LogicaNegocioExcepcion extends Exception {
 
-	private final transient Logger logger = Logger.getLogger(this.getClass());
-	
+//	private final transient Logger logger = Logger.getLogger(this.getClass());
+	Logger logger = Logger.getLogger(this.getClass());
 	public LogicaNegocioExcepcion(String mensajeExcepcion) {
 		super(mensajeExcepcion);
 		logger.error(mensajeExcepcion);
 	}
+	public LogicaNegocioExcepcion(String message, Throwable cause) {
+		super(message, cause);
+		logger.error(message);
+		// TODO Auto-generated constructor stub
+	}
+	public LogicaNegocioExcepcion(Throwable cause) {
+		super(cause);
+		logger.error(cause.getMessage());
+		// TODO Auto-generated constructor stub
+	}
+
 	
 }
