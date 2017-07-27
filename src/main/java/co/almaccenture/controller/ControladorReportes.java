94542d28aa7d7656b15abb8c02b900f76fcbf80f
Juan Camilo Restrepo <petoandroidz@gmail.com>
2017-07-27 13:58:18 -0500

    Controlador reportes por fecha terminado, vista reportes ventas por fecha casi listo

--- src/main/java/co/almaccenture/controller/ControladorReportes.java
+++ src/main/java/co/almaccenture/controller/ControladorReportes.java
@@ -1,18 +1,29 @@
 package co.almaccenture.controller;
 
+import java.rmi.RemoteException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import co.almaccenture.business.LogicaNegocioProducto;
 
 import co.almaccenture.business.LogicaNegocioVenta;
+import co.almaccenture.exception.LogicaNegocioExcepcion;
 import co.almaccenture.model.DetalleVenta;
 import co.almaccenture.model.Venta;
 
@@ -27,7 +38,12 @@ public class ControladorReportes {
 	@Autowired
 	private LogicaNegocioVenta ventaBL;
 		
+	public static final String MENSAJE_FECHA_VACIA = "La fecha está vacia";
+
+	
 	private Venta venta;
+	
+	private Page<Venta> ventas; 
 
 
 	// @RequestMapping(method = RequestMethod.GET, value = "/reportes")
@@ -65,7 +81,7 @@ public class ControladorReportes {
 
 	
 	@RequestMapping(value = "/reportes/ventas")
-	public ModelAndView listarVentasPorFecha(Pageable pageable) { //metodo
+	public ModelAndView ventasPorFecha(Pageable pageable) { //metodo
 		ModelAndView mav = new ModelAndView("reportesVentas"); // constructor , html
 		try {
 			venta=new Venta();
@@ -75,9 +91,48 @@ public class ControladorReportes {
 		}
 		venta.setDetalles(new ArrayList<>());
 		mav.addObject("detalles", new DetalleVenta());
-		mav.addObject("venta", venta);
+		mav.addObject("ventas", ventas);
 
 		return mav;
+	}
+	
+	@RequestMapping(value = "/reportes/ventas/lista", params={"fechaInicio", "fechaFinal"})
+	public ModelAndView agregarVentasPorFecha(HttpServletRequest req, RedirectAttributes redirect, Pageable pageable) throws RemoteException{
+		String message="";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+try{
+			String fechaInicial = req.getParameter("fechaInicio");
+			String fechaFinal = req.getParameter("fechaFinal");
+			
+			if(fechaInicial.equals("")) throw new LogicaNegocioExcepcion(MENSAJE_FECHA_VACIA);
+			if(fechaFinal.equals("")) throw new LogicaNegocioExcepcion(MENSAJE_FECHA_VACIA);
+			fechaInicial=fechaInicial.replace("-", "/");
+			fechaFinal=fechaFinal.replace("-", "/");
+			java.util.Date date1 = sdf.parse(fechaInicial);
+			java.util.Date date2 = sdf.parse(fechaFinal);
+			
+			Date sqldate1=new Date(date1.getTime());
+			Date sqldate2=new Date(date2.getTime());
+			
+			ventas=ventaBL.obtenerVentasPorFecha(sqldate1, sqldate2, pageable);
+			System.out.println("Entre las fechas: " + sqldate1 + " y " + sqldate2 + " se ENCONTRARON: " + ventas.getTotalElements() 
+			+ " registros de venta");
+		
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			message=e.getMessage();
+		}
+		
+
+		ModelAndView mav= new ModelAndView("redirect:/reportes/ventas");
+		mav.addObject("ventas", ventas.getContent());
+		redirect.addFlashAttribute("message", message);
+		
+		return mav;
+	
+		
+			
 	}
 	
 
