b8f2ca88f76503c670a13aca0a166fe93d712ded
Juan Camilo Restrepo <petoandroidz@gmail.com>
2017-07-27 10:01:10 -0500

    Inicio controlador reportes por fecha / Modificaciones a vista reportes por fecha

--- src/main/java/co/almaccenture/controller/ControladorReportes.java
+++ src/main/java/co/almaccenture/controller/ControladorReportes.java
@@ -1,4 +1,7 @@
 package co.almaccenture.controller;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
@@ -6,13 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import co.almaccenture.business.LogicaNegocioProducto;
+import co.almaccenture.business.LogicaNegocioVenta;
+import co.almaccenture.model.DetalleVenta;
+import co.almaccenture.model.Venta;
+
 
 @Controller
 public class ControladorReportes {
 	
 	@Autowired
 	private LogicaNegocioProducto producto;
-	
+	@Autowired
+	private LogicaNegocioVenta ventaBL;
+		
+	private Venta venta;
 	@RequestMapping(method = RequestMethod.GET, value = "/reportes")
 	public ModelAndView listar(Pageable pageable) { //metodo
 		ModelAndView mav = new ModelAndView("reportesAgotados"); // constructor , html
@@ -22,6 +32,22 @@ public class ControladorReportes {
 		
 			e.printStackTrace();
 		}
+		return mav;
+	}
+	
+	@RequestMapping(value = "/reportes/ventas")
+	public ModelAndView listarVentasPorFecha(Pageable pageable) { //metodo
+		ModelAndView mav = new ModelAndView("reportesVentas"); // constructor , html
+		try {
+			venta=new Venta();
+		} catch (Exception e) {
+		
+			e.printStackTrace();
+		}
+		venta.setDetalles(new ArrayList<>());
+		mav.addObject("detalles", new DetalleVenta());
+		mav.addObject("venta", venta);
+
 		return mav;
 	}
 	
